31180c31405a040002c7393577a9a88a499e6c60
wqkenqing <wqkenqingto@163.com>
2017-09-15 15:53:57 +0800

    update
    引入总结的mr的模板，开发ip查询接口和ip统计功能。

---
+++ minebackup/src/main/java/com/minecode/template/mr/TemplateMuti.java
@@ -0,0 +1,144 @@
+package com.minecode.template.mr;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.type.TypeReference;
+
+import java.io.IOException;
+import java.net.URI;
+
+/**
+* @Author wangkuiqing
+* @Date 2016/12/5 16:58
+* @ClassName:TemplateMuti
+* @Description:mr多路径输出模板总结，主要分为addNamedOutput与basePathoutput
+*/
+public class TemplateMuti implements Tool {
+    static String FILE_ROOT = "";
+    static String FILE_INPUT = "";
+    static String FILE_OUTPUT = "";
+
+    public static void main(String[] args) throws Exception {
+
+        ToolRunner.run(new TemplateMuti(), args);
+    }
+
+    @Override
+    public int run(String[] args) throws Exception {
+
+        FILE_ROOT = args[0];
+        FILE_INPUT = args[1];
+        FILE_OUTPUT = args[2];
+
+
+        Configuration conf = new Configuration();
+
+        FileSystem fileSystem = FileSystem.get(new URI(FILE_ROOT), conf);
+        Path outpath = new Path(FILE_OUTPUT);
+        if (fileSystem.exists(outpath)) {
+            fileSystem.delete(outpath, true);
+        }
+
+        // 0 定义干活的人
+        Job job = new Job(conf);
+        // 打包运行必须执行的方法
+        job.setJarByClass(TemplateMuti.class);
+        // 1.1 告诉干活的人 输入流位置 读取hdfs中的文件。每一行解析成一个<k,v>。每一个键值对调用一次map函数
+        FileInputFormat.setInputPaths(job, FILE_INPUT);
+        // 指定如何对输入文件进行格式化，把输入文件每一行解析成键值对
+        job.setInputFormatClass(TextInputFormat.class);
+
+        // 1.2 指定自定义的map类
+        job.setMapperClass(TemplateMutiMapper.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(Text.class);
+
+        // 1.3 分区
+        job.setNumReduceTasks(1);
+
+        // 1.4 TODO 排序、分组 目前按照默认方式执行
+        // 1.5 TODO 规约
+
+        // 2.2 指定自定义reduce类
+        job.setReducerClass(TemplateMutiReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+
+        // 2.3 指定写出到哪里
+        FileOutputFormat.setOutputPath(job, outpath);
+        job.setOutputFormatClass(TextOutputFormat.class);
+
+        // 让干活的人干活
+        job.waitForCompletion(true);
+        return 0;
+    }
+
+    @Override
+    public void setConf(Configuration configuration) {
+
+    }
+
+    @Override
+    public Configuration getConf() {
+        return null;
+    }
+}
+
+class TemplateMutiMapper extends Mapper<LongWritable, Text, Text, Text> {
+
+
+    {
+
+    }
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String line=  value.toString();
+        String []ls=line.split("\t");
+
+    }
+}
+
+class TemplateMutiReducer extends Reducer<Text, Text, Text, Text> {
+    private MultipleOutputs outputs;
+    @Override
+    public void setup(Context context) {
+     outputs = new MultipleOutputs(context);
+    }
+
+    @Override
+    protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        for(Text value:values){
+            outputs.write(key,value,key.toString());
+        }
+    }
+    @Override
+    protected void cleanup(Context context) throws IOException, InterruptedException {
+        outputs.close();
+    }
+    public static <T> T jsonToGenericObject(String jsonString, TypeReference<T> tr) {
+        ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+        if (StringUtils.isBlank(jsonString)) {
+            return null;
+        } else try {
+            return OBJECT_MAPPER.readValue(jsonString, tr);
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+    }
+}
---
+++ minebackup/src/main/java/com/minecode/test/Test.java
@@ -0,0 +1,10 @@
+package com.minecode.test;
+
+/**
+ * Created by wqkenqing on 2017/9/15.
+ */
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
