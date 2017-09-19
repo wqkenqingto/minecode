331c67a23ddb131cf96b089acd5eac75240d4933
wqkenqing <wqkenqingto@163.com>
2017-09-19 15:01:24 +0800

    Spring demo
    尝试尝试搭建了一个spring 样本，为后续敏捷开发作准备

---
+++ minebackup/springstudy/src/main/java/com/minecode/utils/TEst.java
@@ -0,0 +1,10 @@
+package com.minecode.utils;
+
+/**
+ * Created by wqkenqing on 2017/9/19.
+ */
+class Test {
+    public static void main(String[] args) {
+        System.out.println("进入了teat");
+    }
+}
